badc3e3bc28515a3a3a2a60e00dcc92817e10ddc
Charizard17 <3723esad@gmail.com>
2020-06-21 15:02:51 +0200

    image change function added

--- demo.js
+++ demo.js
@@ -28,10 +28,11 @@
     box.setAttribute("id", json[i].id);
     box.setAttribute("onclick", "clickBox(this.id)");
     parent.classList.add("ml-2");
-    parent.setAttribute("id", json[i].id + "child");
+    parent.setAttribute("id", json[i].id + "parent");
     parent.innerHTML = json[i].name;
     if (json[i].children.length > 0) {
       let triangle = document.createElement("span");
+      triangle.setAttribute("id", json[i].id + "triangle");
       // triangle.innerHTML = "▼":
       triangle.innerHTML = "►";
       triangle.classList.add("mr-1");
@@ -39,20 +40,26 @@
       for (let j = 0; j < json[i].children.length; ++j) {
         let listElement2 = document.createElement("div");
         let child = document.createElement("span");
+        let childImgSource = document.createElement("span");
         let box2 = document.createElement("button");
         box2.classList.add("border");
         box2.classList.add("border-secondary");
         box2.classList.add("rounded-sm");
         box2.classList.add("bg-white");
-        box2.style.height = "14px";
         box2.classList.add("ml-5");
+        box2.style.height = "14px";
         box2.setAttribute("id", json[i].children[j].id);
-        child.classList.add("ml-2");
-        listElement2.style.display = "none";
+        box2.setAttribute("onclick", "selectImage(this.id)");
         child.innerHTML = json[i].children[j].name;
+        child.classList.add("ml-2");
+        childImgSource.innerHTML = json[i].children[j].image;
+        childImgSource.setAttribute("id", json[i].children[j].id + "img");
+        childImgSource.style.display = "none";
         listElement2.appendChild(box2);
         listElement2.appendChild(child);
+        listElement2.appendChild(childImgSource);
         parent.appendChild(listElement2);
+        listElement2.style.display = "none";
       }
     }
     listElement.appendChild(box);
@@ -61,30 +68,39 @@
   }
 
   let image = document.createElement("IMG");
-  source = json[0].children[0].image;
-  image.src = source;
   image.classList.add("img-thumbnail");
+  image.setAttribute("id", "ImageElement");
+  image.src = "";
   imgDiv.appendChild(image);
-
   data.appendChild(listDiv);
   data.appendChild(imgDiv);
 })();
 
 function clickBox(id) {
-  var selectedBox = document.getElementById(id);
-  console.log(selectedBox);
-  var clickedElement = document.getElementById(id + "child").childNodes;
-  console.log(clickedElement);
+  let selectedBox = document.getElementById(id);
+  let selectTriangle = document.getElementById(id + "triangle");
+  let clickedElement = document.getElementById(id + "parent").childNodes;
 
   if (clickedElement.length > 1) {
-    for (var i = 1; i < clickedElement.length; ++i) {
+    for (let i = 1; i < clickedElement.length; ++i) {
       if (clickedElement[i].style.display === "none") {
         clickedElement[i].style.display = "";
-        selectedBox.classList.add("bg-info");
+        selectTriangle.innerHTML = "▼";
+        selectedBox.classList.remove("bg-white");
+        selectedBox.classList.add("bg-success");
       } else {
         clickedElement[i].style.display = "none";
+        selectTriangle.innerHTML = "►";
+        selectedBox.classList.remove("bg-success");
         selectedBox.classList.add("bg-white");
       }
     }
   }
+}
+
+function selectImage(id) {
+  let imageSource = document.getElementById(id + "img").innerText;
+  let imageElement = document.getElementById("ImageElement");
+  imageElement.src = imageSource;
+
 }
